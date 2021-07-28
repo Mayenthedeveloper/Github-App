d1ab5a729b5e0b7bd2fb62058e0f727907a4a270
Mayen Akpan <neroxma@yahoo.com>
2021-07-27 19:41:23 -0700

    Update

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,10 +1,34 @@
 import React, { useContext } from "react";
+import { isDOMComponentElement } from "react-dom/test-utils";
 import { GithubContext } from "../context/GithubContext";
 
 function Overview() {
   const { overview } = useContext(GithubContext);
+  let id = 0;
 
-  return <div className="overviewContainer"></div>;
+  return (
+    <div className="overviewContainer">
+      {overview &&
+        overview.map((item, index) => (
+          <a
+            href={item.html_url}
+            target="_blank"
+            key={index}
+            rel="noopener noeferrer"
+          >
+            <div className={"overview overview" + ++id}>
+              {item.name && <h2>{item.name}</h2>}
+              {item.language && (
+                <p>
+                  <span></span>
+                  {item.language}
+                </p>
+              )}
+            </div>
+          </a>
+        ))}
+    </div>
+  );
 }
 
 export default Overview;
